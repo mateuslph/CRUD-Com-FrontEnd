9909b66587acc5a9a9d49ef123e93a0fb6f0f291
mateuslph <mlptche@gmail.com>
2023-10-02 21:28:52 -0300

    implementado os dois primeiros todo

--- src/main/java/com/webAplication/screenmatch/business/FilmesBusiness.java
+++ src/main/java/com/webAplication/screenmatch/business/FilmesBusiness.java
@@ -11,12 +11,21 @@ public class FilmesBusiness {
         if (filme.getDuracaoEmMinutos() < 100) {
             mensagens.add("Filme com Duração menor que o pemitido!!");
         }
+        if (filme.getAnoLancamento() < 1900) {
+            mensagens.add("Ano menor que 1900 não é permitido!!");
+        }
+        if (filme.getGenero().toUpperCase().equals("AÇÃO")) {
+                if ((filme.getDuracaoEmMinutos() < 120) && (filme.getAnoLancamento() < 1950)) {
+                    mensagens.add("o genero de acao nao seja com duracao menor de que 120 minutos e\n" +
+                            "o ano menor que 1950");
+                }
+        }
     }
     // TODO: 01/10/2023 validar para que o filme nao seja menor que 1900 caso for ficar na mesma tela e
-    //  avisar a inconsistemncia ao usuario
+    //  avisar a inconsistemncia ao usuario --> ok
 
     // TODO: 01/10/2023 validar para que o genero de acao nao seja com duracao menor de que 120 minutos e
-    //  o ano menor que 1950
+    //  o ano menor que 1950 --> ok
 
     // TODO: 01/10/2023 mostrar essas inconsistencias na tela
 }
